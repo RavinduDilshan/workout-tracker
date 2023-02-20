390dd80906cf6b475c86e4d330940b8be5d3bd01
Ravidu Dilshan <36620171+RavinduDilshan@users.noreply.github.com>
2023-02-20 12:16:47 +0530

    date time convert functions added

--- lib/datetime/date_time.dart
+++ lib/datetime/date_time.dart
@@ -1,4 +1,6 @@
 //return today date as yyyymmdd
+import 'package:flutter/material.dart';
+
 String todaysDateYYYYMMDD() {
   //today
   var dateTimeObject = DateTime.now();
@@ -24,5 +26,32 @@ String todaysDateYYYYMMDD() {
 }
 
 //convert string yyyymmdd to DateTime object
+DateTime createDateTimeObject(String yyyymmdd) {
+  int yyyy = int.parse(yyyymmdd.substring(0, 4));
+  int mm = int.parse(yyyymmdd.substring(4, 6));
+  int dd = int.parse(yyyymmdd.substring(6, 8));
+  DateTime dateTimeObject = DateTime(yyyy, mm, dd);
+  return dateTimeObject;
+}
 
 //convert DateTime object to yyyymmdd string
+String convertDateTimeToYYYYMMDD(DateTime dateTime) {
+   //year in the format YYYY
+  String year = dateTime.year.toString();
+
+  //month in format mm
+  String month = dateTime.month.toString();
+  if (month.length == 1) {
+    month = '0$month';
+  }
+
+  //day in format dd
+  String date = dateTime.day.toString();
+  if (date.length == 1) {
+    date = '0$date';
+  }
+
+  //final format
+  String yyyymmdd = year + month + date;
+  return yyyymmdd;
+}
